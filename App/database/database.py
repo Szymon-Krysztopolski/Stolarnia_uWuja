a9ee0802ee7b09fa7b7338d09a9de042c466bd43
Szymon-Krysztopolski <szymon.krysztopolski@gmail.com>
2022-01-19 21:12:14 +0100

    dodawanie, usuwanie, modyfikacja
    produkty i materialy

--- App/database/database.py
+++ App/database/database.py
@@ -14,3 +14,53 @@ def MyQuery(q,vars=-1):
     except (connection.Error, connection.Warning) as e:
         print(e)
         return None
+    
+class Rodzaje_produktow():
+    def create_table():
+        table_script = '''CREATE TABLE IF NOT EXISTS Rodzaje_produktow(
+                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nazwa VARCHAR(50) not null
+                    );
+                    '''
+        MyQuery(table_script)
+
+    def insert_record(nazwa):
+        MyQuery('INSERT INTO Rodzaje_produktow(nazwa) VALUES(?)',(nazwa,))
+        
+    def update_record(id,nazwa):
+        MyQuery('UPDATE Rodzaje_produktow SET nazwa = ? where ID= ?',(nazwa,id))
+        
+class Rodzaje_materialow():
+    def create_table():
+        table_script = '''CREATE TABLE IF NOT EXISTS Rodzaje_materialow(
+                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nazwa VARCHAR(50) not null,
+                        cena_za_jednostke number(10) not null
+                    );
+                    '''
+        MyQuery(table_script)
+
+    def insert_record(nazwa,cena):
+        MyQuery('INSERT INTO Rodzaje_materialow(nazwa,cena_za_jednostke) VALUES(?,?)',(nazwa,cena))
+        
+    def update_record(id,nazwa,cena):
+        MyQuery('UPDATE Rodzaje_materialow SET nazwa=?, cena_za_jednostke=? where ID=?',(nazwa,cena,id))
+        
+def fetch_records(table):
+    return MyQuery(f"SELECT * FROM {table}")
+
+def del_record_byID(table,id):
+    MyQuery(f"DELETE FROM {table} where id={id}")
+
+def database_init():
+    Rodzaje_produktow.create_table()
+    Rodzaje_materialow.create_table()
+    
+def database_hard_reset():
+    try:
+        cursor.execute("DROP TABLE Rodzaje_produktow")
+        cursor.execute("DROP TABLE Rodzaje_materialow")
+        connection.commit()
+    except (connection.Error, connection.Warning) as e:
+        print(e)
+        return None
--- App/web_server.py
+++ App/web_server.py
@@ -1,20 +1,41 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from sqlite3 import connect
+from urllib.parse import urlparse, parse_qs
+
+from matplotlib.cbook import normalize_kwargs
 import database.database as db
+import cgi
+import pyautogui
 
 HOST_NAME = "127.0.0.1"
 PORT = 8404
 
 PATH_HTML = "./templates"
 
+def butt_del(table,id):
+    table=table[:2]
+    return f'''<form method="POST", enctype="multipart/form-data" action="/del_{table}_{id}">
+            <input type="submit" value="Usun">
+        </form>'''
+
+def butt_mod(table,id):
+    table=table[:2]
+    return f'''<form method="POST", enctype="multipart/form-data" action="/mod_{table}_{id}">
+            <input type="submit" value="Modyfikuj">
+        </form>'''
+
 def read_html_template(path):
-    # function to read HTML file
     try:
         with open(path) as f:
             file = f.read()
     except Exception as e:
         file = e
     return file
+
+def print_records(q):
+    rows = q.fetchall()
+    for row in rows:
+        print(row)
 
 class PythonServer(SimpleHTTPRequestHandler):
     def respond(self, file):
@@ -23,14 +44,115 @@ class PythonServer(SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(tmp, "utf-8"))
         
-    def do_GET(self):
-        global game_is_on, game_time, game_finished, player_status, match_id, players, game_start
-        if self.path == '/':
-            self.respond(PATH_HTML+"/index.html")
+    def show_records(self,path,table_name,table_data):
+        file = read_html_template(path)
 
+        table_row = ""
+        for data in table_data:
+            table_row += "<tr>"
+            for item in data:
+                table_row += "<td>"
+                table_row += str(item)
+                table_row += "</td>"
+                
+            #modif butt
+            table_row += "<td>"
+            #table_row += butt_mod(table_name,str(data[0]))
+            table_row += f'<a class="button" href="/mod_{table_name}_{str(data[0])}">Modyfikuj</a><br>'
+            table_row += "</td>"
+            
+            #del butt
+            table_row += "<td>"
+            table_row += butt_del(table_name,str(data[0]))
+            table_row += "</td>"
+            
+            table_row += "</tr>"
+        file = file.replace("{{records}}", table_row)
+        self.send_response(200, "OK")
+        self.end_headers()
+        self.wfile.write(bytes(file, "utf-8"))
+        
+    def do_GET(self):
+        if self.path == '/' or self.path == '/?':
+            self.respond(PATH_HTML+"/index.html")
+        elif self.path=='/show_ProductTypes':
+            self.show_records(PATH_HTML+"/show_productTypes.html","RP",db.fetch_records("Rodzaje_produktow"))
+        elif self.path=='/show_MaterialTypes':
+            self.show_records(PATH_HTML+"/show_materialTypes.html","RM",db.fetch_records("Rodzaje_materialow"))
+        elif self.path[:4]=='/mod':
+            #mod_??_{id}
+            tab=self.path[5:7]
+            
+            if not '?' in self.path:            
+                self.respond(PATH_HTML+f"/mod_{tab}.html")
+            else:
+                query_components = parse_qs(urlparse(self.path).query)
+                id=self.path[8:self.path.index('?')]
+                if tab=="RP":
+                    nazwa = query_components["prodType"][0]
+                    db.Rodzaje_produktow.update_record(id,nazwa)
+                    self.path="/show_ProductTypes"
+                elif tab=="RM":
+                    nazwa = query_components["matType"][0]
+                    cena = query_components["matPrice"][0]
+                    db.Rodzaje_materialow.update_record(id,nazwa,cena)
+                    self.path="/show_MaterialTypes"
+                    
+                tmp=f'<html><head><meta charset="UTF-8"/><script>window.location.href="{self.path}"</script></head><body></body></html>'
+                self.send_response(200, "OK")
+                self.end_headers()
+                self.wfile.write(bytes(tmp, "utf-8"))
+
+    def do_POST(self):
+        if self.path=='/add_new_productType':
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            pdict['boundary'] = bytes(pdict['boundary'], 'utf-8')
+
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                nazwa = fields.get("prodType")[0]
+                db.Rodzaje_produktow.insert_record(nazwa)
+
+            self.path="/show_ProductTypes"
+            pyautogui.hotkey('f5')
+            
+        elif self.path=='/add_new_materialType':
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            pdict['boundary'] = bytes(pdict['boundary'], 'utf-8')
+
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                nazwa = fields.get("matType")[0]
+                cena = fields.get("matPrice")[0]
+                db.Rodzaje_materialow.insert_record(nazwa,cena)
+
+            self.path="/show_MaterialTypes"
+            pyautogui.hotkey('f5')
+            
+        elif self.path[:4]=='/del':
+            #del_??_{id}
+            tab=self.path[5:7]
+            id=self.path[8:]
+    
+            if tab=="RP":
+                table="Rodzaje_produktow"
+            elif tab=="RM":
+                table="Rodzaje_materialow"
+                
+            db.del_record_byID(table,id)
+            self.path="/show_ProductTypes"
+            pyautogui.hotkey('f5')
+                        
+        
 def server_start():
     db.connection = connect(db.DB_NAME)
     db.cursor = db.connection.cursor()
+    #db.database_hard_reset()
+    db.database_init()
+    
+    #db.Rodzaje_produktow.insert_record("Palety")
+    #db.Rodzaje_produktow.insert_record("Skrzynie")
+    #db.MyQuery("Select * from Rodzaje_produktow")
     
     server = HTTPServer((HOST_NAME, PORT), PythonServer)
     print(f"Server started http://{HOST_NAME}:{PORT}")
@@ -42,3 +164,4 @@ def server_start():
         db.connection.close()
         server.server_close()
         print("Server stopped successfully")
+        
