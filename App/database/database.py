7c7dabff22e78c13620bca2942606b56f26797fa
Szymon-Krysztopolski <szymon.krysztopolski@gmail.com>
2022-01-20 21:13:27 +0100

    produkty + css

--- App/database/database.py
+++ App/database/database.py
@@ -31,10 +31,21 @@ class Rodzaje_produktow():
         MyQuery('UPDATE Rodzaje_produktow SET nazwa=? WHERE ID=?',(nazwa,id))
     
     def get_id_by_name(nazwa):
-        return MyQuery('SELECT ID FROM Rodzaje_produktow WHERE nazwa=?',(nazwa,)).fetchone()[0]
+        res = MyQuery('SELECT ID FROM Rodzaje_produktow WHERE nazwa=?',(nazwa,)).fetchone()
+        
+        if res is not None:
+            return res[0]
+        else:
+            print("Error with query")
+            return None
     
     def get_name_by_id(id):
-        return MyQuery('SELECT nazwa FROM Rodzaje_produktow WHERE ID=?',(id,)).fetchone()[0]
+        res = MyQuery('SELECT nazwa FROM Rodzaje_produktow WHERE ID=?',(id,)).fetchone()
+        if res is not None:
+            return res[0]
+        else:
+            print("Error with query")
+            return None
         
 class Rodzaje_materialow():
     def create_table():
@@ -52,12 +63,21 @@ class Rodzaje_materialow():
     def update_record(id,nazwa,cena):
         MyQuery('UPDATE Rodzaje_materialow SET nazwa=?, cena_za_jednostke=? where ID=?',(nazwa,cena,id))
         
+    def get_id_by_name(nazwa):
+        res = MyQuery('SELECT ID FROM Rodzaje_materialow WHERE nazwa=?',(nazwa,)).fetchone()
+        
+        if res is not None:
+            return res[0]
+        else:
+            print("Error with query")
+            return None
+        
 class Produkty():
     def create_table():
         table_script = '''CREATE TABLE IF NOT EXISTS Produkty(
                         ID INTEGER PRIMARY KEY AUTOINCREMENT,
                         ID_rodzaje_prod INTEGER not null,
-                        nazwa TEXT not null,
+                        nazwa TEXT not null UNIQUE,
                         cena_robocizna INTEGER not null,
                         FOREIGN KEY(ID_rodzaje_prod) REFERENCES Rodzaje_produktow(ID)
                     );
@@ -66,6 +86,35 @@ class Produkty():
 
     def insert_record(ID_rodzaje_prod,nazwa,cena_robocizna):
         MyQuery('INSERT INTO Produkty(ID_rodzaje_prod,nazwa,cena_robocizna) VALUES(?,?,?)',(ID_rodzaje_prod,nazwa,cena_robocizna))
+
+    def update_record(id,id_pt,nazwa,cena):
+        MyQuery('UPDATE Produkty SET ID_rodzaje_prod=?, nazwa=?, cena_robocizna=? where ID=?',(id_pt,nazwa,cena,id))
+
+class Komponenty():
+    def create_table():
+        table_script = '''CREATE TABLE IF NOT EXISTS Komponenty(
+                        ID_produktu INTEGER not null,
+                        ID_materialu INTEGER not null,
+                        nazwa TEXT not null,
+                        wymiar_X INTEGER not null,
+                        wymiar_Y INTEGER not null,
+                        wymiar_Z INTEGER not null,
+                        FOREIGN KEY(ID_produktu) REFERENCES Rodzaje_produktow(ID) on delete cascade,
+                        FOREIGN KEY(ID_materialu) REFERENCES Rodzaje_materialow(ID),
+                        PRIMARY KEY(ID_produktu,nazwa)
+                    );
+                    '''
+        MyQuery(table_script)
+        
+    def insert_record(ID_produktu,ID_materialu,nazwa,wymiar_X,wymiar_Y,wymiar_Z):
+        MyQuery('INSERT INTO Komponenty(ID_produktu,ID_materialu,nazwa,wymiar_X,wymiar_Y,wymiar_Z) VALUES(?,?,?,?,?,?)',
+                (ID_produktu,ID_materialu,nazwa,wymiar_X,wymiar_Y,wymiar_Z))
+        
+    def fetch_records_by_prodID(id):
+        return MyQuery(f"SELECT * FROM Komponenty where ID_produktu={id}")
+    
+    def del_record_byID(id1, id2, name):
+        MyQuery("DELETE FROM Komponenty where ID_produktu=? and ID_materialu=? and nazwa=?",(id1, id2, name))
 
 def fetch_records(table):
     return MyQuery(f"SELECT * FROM {table}")
@@ -77,6 +126,7 @@ def database_init():
     Rodzaje_produktow.create_table()
     Rodzaje_materialow.create_table()
     Produkty.create_table()
+    Komponenty.create_table()
     
 def database_hard_reset():
     try:
@@ -84,6 +134,7 @@ def database_hard_reset():
         cursor.execute("DROP TABLE Rodzaje_produktow")
         cursor.execute("DROP TABLE Rodzaje_materialow")
         cursor.execute("DROP TABLE Produkty")
+        cursor.execute("DROP TABLE Komponenty")
         database_init()
         connection.commit()
     except (connection.Error, connection.Warning) as e:
--- App/web_server.py
+++ App/web_server.py
@@ -11,17 +11,14 @@ HOST_NAME = "127.0.0.1"
 PORT = 8404
 
 PATH_HTML = "./templates"
-
-def butt_del(table,id):
+#-------------------------------------------------------------------------
+def butt_del(table,id,id2,name):
     table=table[:2]
-    return f'''<form method="POST", enctype="multipart/form-data" action="/del_{table}_{id}">
+    if table!="Ko":
+        id2=""
+        
+    return f'''<form method="POST", enctype="multipart/form-data" action="/del_{table}_{id}-{id2}*{name}">
             <input type="submit" value="Usun">
-        </form>'''
-
-def butt_mod(table,id):
-    table=table[:2]
-    return f'''<form method="POST", enctype="multipart/form-data" action="/mod_{table}_{id}">
-            <input type="submit" value="Modyfikuj">
         </form>'''
 
 def read_html_template(path):
@@ -38,15 +35,26 @@ def print_records(q):
         print(row)
 
 class PythonServer(SimpleHTTPRequestHandler):
-    def respond(self, file):
-        tmp=read_html_template(file)
+    def respond(self, html):
+        tmp=read_html_template(html)
+        tmp = self.import_css(tmp)
+        
         self.send_response(200, "OK")
         self.end_headers()
         self.wfile.write(bytes(tmp, "utf-8"))
         
+    def import_css(self,html):
+        try:
+            with open(PATH_HTML+"/style/style.css") as f:
+                file = f.read()
+        except Exception as e:
+            file = e
+        return html.replace("{{MyStyle}}", file)
+        
     def show_records(self,path,table_name,table_data):
         file = read_html_template(path)
-
+        file = self.import_css(file)
+        
         table_row = ""
         for data in table_data:
             table_row += "<tr>"
@@ -56,22 +64,31 @@ class PythonServer(SimpleHTTPRequestHandler):
                 table_row += "</td>"
                 
             #modif butt
-            table_row += "<td>"
-            #table_row += butt_mod(table_name,str(data[0]))
-            table_row += f'<a class="button" href="/mod_{table_name}_{str(data[0])}">Modyfikuj</a><br>'
-            table_row += "</td>"
+            if table_name != "Ko":
+                table_row += "<td>"
+                table_row += f'<a class="button" href="/mod_{table_name}_{str(data[0])}">Modyfikuj</a><br>'
+                table_row += "</td>"
             
             #del butt
             table_row += "<td>"
-            table_row += butt_del(table_name,str(data[0]))
+            if table_name == "Ko":
+                table_row += butt_del(table_name,str(data[0]),str(data[1]),str(data[2]))
+            else:
+                table_row += butt_del(table_name,str(data[0]),"","")
             table_row += "</td>"
+            
+            #details for components
+            if table_name=="Pr":
+                table_row += "<td>"
+                table_row += f'<a class="button" href="/det_{table_name}_{str(data[0])}">Szczegoly</a><br>'
+                table_row += "</td>"
             
             table_row += "</tr>"
         file = file.replace("{{records}}", table_row)
         self.send_response(200, "OK")
         self.end_headers()
         self.wfile.write(bytes(file, "utf-8"))
-        
+#-------------------------------------------------------------------------
     def do_GET(self):
         if self.path == '/' or self.path == '/?':
             self.respond(PATH_HTML+"/index.html")
@@ -99,12 +116,46 @@ class PythonServer(SimpleHTTPRequestHandler):
                     cena = query_components["matPrice"][0]
                     db.Rodzaje_materialow.update_record(id,nazwa,cena)
                     self.path="/show_MaterialTypes"
+                elif tab=="Pr":
+                    nazwa = query_components["prodName"][0]
+                    nazwa_pt = query_components["prodTypeName"][0]
+                    cena = query_components["prodPrice"][0]
+                    
+                    id_pt=db.Rodzaje_produktow.get_id_by_name(nazwa_pt)
+                    if id_pt is not None:
+                        db.Produkty.update_record(id,id_pt,nazwa,cena)
+                    self.path="/show_Products"
                     
                 tmp=f'<html><head><meta charset="UTF-8"/><script>window.location.href="{self.path}"</script></head><body></body></html>'
                 self.send_response(200, "OK")
                 self.end_headers()
                 self.wfile.write(bytes(tmp, "utf-8"))
-
+                
+        elif self.path[:4]=='/det':
+            tab=self.path[5:7]
+            
+            if not '?' in self.path:
+                id=self.path[8:]
+                self.show_records(PATH_HTML+"/det_Pr.html","Ko",db.Komponenty.fetch_records_by_prodID(id))
+            else:
+                id=self.path[8:self.path.index('?')]
+                query_components = parse_qs(urlparse(self.path).query)
+                nazwa = query_components["compName"][0]
+                nazwa_mat = query_components["matName"][0]
+                wymX = query_components["dimX"][0]
+                wymY = query_components["dimY"][0]
+                wymZ = query_components["dimZ"][0]
+                
+                id_mat=db.Rodzaje_materialow.get_id_by_name(nazwa_mat)
+                if id_mat is not None:
+                    db.Komponenty.insert_record(id,id_mat,nazwa,wymX,wymY,wymZ)
+                
+                self.path=f"/det_Pr_{id}"
+                tmp=f'<html><head><meta charset="UTF-8"/><script>window.location.href="{self.path}"</script></head><body></body></html>'
+                self.send_response(200, "OK")
+                self.end_headers()
+                self.wfile.write(bytes(tmp, "utf-8"))
+#-------------------------------------------------------------------------
     def do_POST(self):
         if self.path=='/add_new_productType':
             ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
@@ -136,27 +187,31 @@ class PythonServer(SimpleHTTPRequestHandler):
                 nazwa_pt = fields.get("prodTypeName")[0]
                 nazwa = fields.get("prodName")[0]
                 cena = fields.get("prodPrice")[0]
-                
+
                 id_pt=db.Rodzaje_produktow.get_id_by_name(nazwa_pt)
-                print(id_pt)
-                db.Produkty.insert_record(id_pt,nazwa,cena)
+                if id_pt is not None:
+                    db.Produkty.insert_record(id_pt,nazwa,cena)
             pyautogui.hotkey('f5')
             
         elif self.path[:4]=='/del':
             #del_??_{id}
             tab=self.path[5:7]
-            id=self.path[8:]
+            id=self.path[8:self.path.index('-')]
+            id2=self.path[self.path.index('-')+1:self.path.index('*')]
+            name=self.path[self.path.index('*')+1:]
+            
     
             if tab=="RP":
-                table="Rodzaje_produktow"
+                db.del_record_byID("Rodzaje_produktow",id)
             elif tab=="RM":
-                table="Rodzaje_materialow"
+                db.del_record_byID("Rodzaje_materialow",id)
             elif tab=="Pr":
-                table="Produkty"
+                db.del_record_byID("Produkty",id)
+            elif tab=="Ko":
+                db.Komponenty.del_record_byID(id,id2,name)
                 
-            db.del_record_byID(table,id)
             pyautogui.hotkey('f5')
-        
+#-------------------------------------------------------------------------
 def server_start():
     db.connection = connect(db.DB_NAME)
     db.cursor = db.connection.cursor()
